ade165a2b87b18f748fd72e2d7a175064d283cd4
Pavan Jadda <jaddap2@nih.gov>
2020-05-21 01:45:49 -0400

    Commit

---
+++ src/app/sequence-number.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SequenceNumberService {
+
+  constructor(private http: HttpClient) { }
+
+    loadSequenceNumbers() : Observable<any>
+    {
+      return this.http.get('results.json');
+    }
+}
