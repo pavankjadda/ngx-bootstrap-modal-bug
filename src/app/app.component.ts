bdc9a45d5a68e735fbb56092be3b5299366da008
Pavan Jadda <jaddap2@nih.gov>
2020-05-21 01:45:57 -0400

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit, TemplateRef, VERSION, ViewChild} from '@angular/core';
+import {MatTableDataSource} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
+import {BsModalRef, BsModalService} from "ngx-bootstrap/modal";
+import {SequenceNumberService} from "./sequence-number.service";
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
-  title = 'ngx-bootstrap-modal-bug';
+export class AppComponent  implements OnInit
+{
+  name = 'Angular ' + VERSION.major;
+  sequenceNumbers: SequenceNumber[];
+
+  //Mat Table
+  sequenceNumberDisplayedColumns: string[] = ['personFullName', 'consentDate', 'sequenceNumber'];
+  sequenceNumbersDataSource = new MatTableDataSource<SequenceNumber>();
+  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: false}) sort: MatSort;
+
+  //Ngx-Bootstrap Modal default config
+  modalRef: BsModalRef;
+  modalConfig = {
+    keyboard: false,
+    backdrop: true,
+    ignoreBackdropClick: true,
+    class: 'modal-md'
+  };
+
+  constructor(private sequenceNumberService: SequenceNumberService,
+              private changeDetectorRef: ChangeDetectorRef,
+              private modalService: BsModalService)
+  {
+  }
+
+  ngOnInit(): void
+  {
+
+  }
+
+  openSequenceNumberEditModal(template: TemplateRef<any>)
+  {
+    this.loadSequenceNumbers();
+
+    this.modalRef = this.modalService.show(template, this.modalConfig);
+    this.modalRef.setClass('modal-lg');
+  }
+
+  private loadSequenceNumbers()
+  {
+    this.sequenceNumberService.loadSequenceNumbers().subscribe(
+      data=>
+      {
+        this.sequenceNumbers=data;
+      }
+    )
+  }
+}
+
+export class SequenceNumber
+{
+  personFullName: string;
+  sequenceNumber: number;
+  consentDate: string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import {MatRadioModule} from "@angular/material/radio";
+import {MatButtonModule} from "@angular/material/button";
+import { MatTableModule } from '@angular/material/table';
+import {MatSortModule} from "@angular/material/sort";
+import {MatSelectModule} from "@angular/material/select";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatInputModule} from "@angular/material/input";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatDialogModule} from "@angular/material/dialog";
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatRadioModule,
+    FormsModule,
+    MatButtonModule,
+    MatTableModule,
+    MatSortModule,
+    MatSelectModule,
+    MatGridListModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatDatepickerModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
